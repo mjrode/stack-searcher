af06de4112ced82b720f2a7bee6a66adab75bd8e
mike.rode <mike.rode@malauzai.com>
2018-01-15 20:35:14 -0600

    Add method and tests for seeing avail urls

--- app/services/pagination.rb
+++ app/services/pagination.rb
@@ -5,9 +5,42 @@ class Pagination
     @headers = OpenStruct.new(response.headers)
   end
 
-  def next_page?
+  def more_pages?
     link = @headers.link
     next_link = link.first.include?('next') if link
     next_link ? true : false
+  end
+
+  def next_page_url
+    raw_url = split_urls.select {|url| url.include?('next')}
+    parse_url(raw_url)
+  end
+
+  def available_urls
+    a = @headers['link'].join.split(',').map{|string| string.split("rel=").last}
+
+    first_available = a.any? {|a| a.include?('first')}
+    last_available = a.any?  {|a| a.include?('last')}
+    prev_available = a.any?  {|a| a.include?('prev')}
+    next_available = a.any?  {|a| a.include?('next')}
+
+    {
+      first_available: first_available,
+      last_available: last_available,
+      prev_available: prev_available,
+      next_available: next_available
+    }
+  end
+
+  def links_present?
+    @headers.link ? true : false
+  end
+
+  def parse_url(raw_url)
+    raw_url.join[/<(.*?)>/, 1]
+  end
+
+  def split_urls
+    @headers.link.join('').split(',')
   end
 end
--- test/services/pagination_test.rb
+++ test/services/pagination_test.rb
@@ -6,22 +6,52 @@ class PaginationTest < ActiveSupport::TestCase
 
   end
 
-  test "returns true if there is another page present" do
+  test 'returns true if there are more pages' do
     response = multiple_page_request
     pagination = Pagination.new(response)
-    assert pagination.next_page?
+    assert_equal pagination.more_pages?, true
   end
 
   test 'returns false when there are no more pages' do
     response = single_page_request
     pagination = Pagination.new(response)
-    assert_equal pagination.next_page?, false
+    assert_equal pagination.more_pages?, false
+  end
+
+  # test 'returns next page url when present' do
+  #   response = multiple_page_request
+  #   pagination = Pagination.new(response)
+  #   binding.pry
+  # end
+
+  test 'returns true when there are links present in the headers' do
+    response = last_page_request
+    pagination = Pagination.new(response)
+    assert_equal pagination.links_present?, true
+  end
+
+  test 'parse next url from links in header'  do
+    response = multiple_page_request
+    pagination = Pagination.new(response)
+    url = pagination.next_page_url
+    assert_equal url, 'https://api.github.com/search/code?q=vcr+minitest+filename%3AGemfile.lock&page=2&per_page=100'
+  end
+
+  test 'see which urls are present in headers'  do
+    response = multiple_page_request
+    pagination = Pagination.new(response)
+    url_hash = pagination.available_urls
+
+    assert_equal url_hash[:next_available], true
+    assert_equal url_hash[:last_available], true
+    assert_equal url_hash[:prev_available], false
+    assert_equal url_hash[:first_available], false
   end
 
   private
 
   def last_page_request
-    use_cassette 'code request that returns multiple pages' do
+    use_cassette 'code request that returns last page' do
       response = Common::GithubClient.run(
         api_url: "https://api.github.com/search/code?q=vcr+minitest+filename%3AGemfile.lock&page=10&per_page=100"
       )
